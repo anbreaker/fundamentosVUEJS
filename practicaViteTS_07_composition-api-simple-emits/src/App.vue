2d08ca9dcbc973e880a69fd09016e5cbcdde0270
anbreaker <antunez19@gmail.com>
2022-09-03 09:40:11 +0200

    added simple example emit with ts and new Vue3

---
+++ practicaViteTS_07_composition-api-simple-emits/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <h1>Select a Size: {{ selectedSize }}</h1>
+  <SizeSelector :selectedSize="selectedSize" @onChangeSize="selectedSize = $event" />
+</template>
+
+<script setup lang="ts">
+  import { ref } from 'vue';
+
+  import SizeSelector from './components/SizeSelector.vue';
+
+  const selectedSize = ref('XL');
+</script>
+
+<style>
+  body {
+    background-color: #2c3e50;
+  }
+
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: aliceblue;
+    margin-top: 60px;
+  }
+
+  button {
+    padding: 5px;
+    margin-right: 5px;
+    border: none;
+    border-radius: 5px;
+    width: 75px;
+    cursor: pointer;
+  }
+
+  button:hover {
+    background-color: #3498db;
+    color: aliceblue;
+  }
+</style>
---
+++ practicaViteTS_07_composition-api-simple-emits/src/components/SizeSelector.vue
@@ -0,0 +1,40 @@
+<template>
+  <button
+    v-for="size in posibleSizes"
+    :class="{ active: props.selectedSize === size }"
+    @click="emits('onChangeSize', size)"
+  >
+    {{ size }}
+  </button>
+</template>
+
+<script lang="ts" setup>
+  const posibleSizes = ['XS', 'S', 'M', 'L', 'XL', 'XXL'];
+
+  //JavaScript
+  /*
+  const props = defineProps({
+    selectedSize: {
+      type: String,
+      required: true,
+    },
+  });
+
+  const emits = defineEmits({
+    onChangeSize: String,
+  });
+   */
+
+  //TypeScript
+  const props = defineProps<{ selectedSize: string }>();
+
+  const emits = defineEmits<{ (event: 'onChangeSize', value: string): void }>();
+</script>
+
+<style>
+  .active {
+    background-color: #3498db;
+    color: aliceblue;
+    transition: all 0.3s ease-out;
+  }
+</style>
